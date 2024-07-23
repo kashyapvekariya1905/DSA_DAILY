20de292214ffc362e00b0f99eb16a2e1a25fc381
Kashyap Vekariya <115556362+kashyapvekariya1905@users.noreply.github.com>
2024-07-23 19:54:01 +0530

    Create 23-07M merge 2 BSTs.py

---
+++ GFG DAILY/23-07M merge 2 BSTs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, root1, root2):
+        # code here
+        def inorder(root,ans):
+            if root==None:
+                return 
+            inorder(root.left,ans)
+            ans.append(root.data)
+            inorder(root.right,ans)
+        ans=[]
+        inorder(root1,ans)
+        inorder(root2,ans)
+        ans.sort()
+        return ans
