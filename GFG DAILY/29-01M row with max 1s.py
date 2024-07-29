eb27c367a36bfeb8333a4104e8448df130c65e9c
Kashyap Vekariya <115556362+kashyapvekariya1905@users.noreply.github.com>
2024-07-29 07:26:06 +0530

    Update GFG DAILY

---
+++ GFG DAILY/29-01M row with max 1s.py
@@ -0,0 +1,13 @@
+class Solution:
+
+	def rowWithMax1s(self,arr):
+		# code here
+		max_count, max_index = 0, -1
+        for i in range(len(arr)):
+            if arr[i][-1] > max_count:
+                max_count = arr[i][-1]
+                max_index = i
+            elif arr[i][-1] == max_count:
+                max_index = i if sum(arr[i]) > sum(arr[max_index]) else max_index
+        return max_index
+
