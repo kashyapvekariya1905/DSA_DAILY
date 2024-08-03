ab8a5a70cfffbfd408365c26a7266c4856ab3e12
Kashyap Vekariya <115556362+kashyapvekariya1905@users.noreply.github.com>
2024-08-03 08:57:02 +0530

    Create 03E 1460.py

---
+++ LeetCode/AUG-24/03E 1460.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        if len(target) != len(arr):
+            return False
+        
+        count = Counter(target)
+        for num in arr:
+            count[num] -= 1
+        
+        return all(count[num] == 0 for num in count)
