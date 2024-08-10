ff7f8dd4f570f1d80eb91d268732314309018363
Kashyap Vekariya <115556362+kashyapvekariya1905@users.noreply.github.com>
2024-08-11 01:57:11 +0530

    Update AUG-24

---
+++ GFG DAILY/AUG-24/11M job seq pro.py
@@ -0,0 +1,74 @@
+#User function Template for python3
+'''
+class Job:
+    
+    # Job class which stores profit and deadline.
+    
+    def __init__(self,profit=0,deadline=0):
+        self.profit = profit
+        self.deadline = deadline
+        self.id = 0
+'''        
+
+class Solution:
+    
+    #Function to find the maximum profit and the number of jobs done.
+    def JobScheduling(self,Jobs,n):
+        
+        # code here
+        deadline = 0
+        
+        for each in Jobs:
+            deadline = max(deadline,each.deadline)
+        Jobs.sort(key = lambda x:x.profit,reverse=True)
+        length = len(Jobs)
+        count =0
+        profit = 0
+        dead = [-1]*(deadline+1)
+        for i in range(length):
+            for j in range(Jobs[i].deadline,0,-1):
+                if dead[j]==-1:
+                    profit = profit+Jobs[i].profit
+                    dead[j]=1
+                    count+=1
+                    break
+            
+        return (count,profit)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+
+#Contributed by : Nagendra Jha
+class Job:
+    '''
+    Job class which stores profit and deadline.
+    '''
+
+    def __init__(self, profit=0, deadline=0):
+        self.profit = profit
+        self.deadline = deadline
+        self.id = 0
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        info = list(map(int, input().strip().split()))
+        Jobs = [Job() for i in range(n)]
+        for i in range(n):
+            Jobs[i].id = info[3 * i]
+            Jobs[i].deadline = info[3 * i + 1]
+            Jobs[i].profit = info[3 * i + 2]
+        ob = Solution()
+        res = ob.JobScheduling(Jobs, n)
+        print(res[0], end=" ")
+        print(res[1])
+
+# } Driver Code Ends
