a64b37c74b685263b0e8fcae4521ccc68253c54c
Kashyap Vekariya <115556362+kashyapvekariya1905@users.noreply.github.com>
2024-09-12 05:33:25 +0530

    Create 12E.py

---
+++ GFG DAILY/SEPT-24/12E.py
@@ -0,0 +1,17 @@
+# your task is to complete this function
+
+'''
+class node:
+    def __init__(data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    #  Should return data of middle node. If linked list is empty, then  -1
+    def findMid(self, head):
+        # Code here
+        # return the value stored in the middle node
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        return slow.data
